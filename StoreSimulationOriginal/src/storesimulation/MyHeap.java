c4d7d3b7da4569e2c658fc4ac36f8434ffcdab32
Ronnie Cole <Coler4@xavier.edu>
2021-02-11 17:09:57 -0500

    Added StoreSimulationOriginal

---
+++ StoreSimulationOriginal/src/storesimulation/MyHeap.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storesimulation;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author timmermank1
+ */
+class MyHeap{
+    //You do not need to resize this.
+   Event[] myHeap =  new Event[5000];  
+    
+
+    int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Event remove() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void insert(Event item) {
+       //Event has an isFirst method to help compare event times
+    }
+    
+}
